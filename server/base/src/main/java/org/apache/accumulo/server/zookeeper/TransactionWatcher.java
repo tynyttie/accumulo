1aa60adfe7a9d5e6b0bfd589d2310c143e3b206b
tynyttie <ty_barnes_jobs@yahoo.com>
2020-09-17 12:25:16 -0400

    Testing stripedLock

--- server/base/src/main/java/org/apache/accumulo/server/zookeeper/TransactionWatcher.java
+++ server/base/src/main/java/org/apache/accumulo/server/zookeeper/TransactionWatcher.java
@@ -40,110 +40,122 @@ import com.google.common.util.concurrent.Striped;
 
 public class TransactionWatcher {
 
-  private static final Logger log = LoggerFactory.getLogger(TransactionWatcher.class);
-  private Striped<Lock> stripedLocks = Striped.lock(1);
+    private static final Logger log = LoggerFactory.getLogger(TransactionWatcher.class);
+    private Striped<Lock> stripedLocks = Striped.lock(1);
 
-  final private Map<Long,AtomicInteger> counts = new HashMap<>();
-  final private Arbitrator arbitrator;
+    final private Map<Long, AtomicInteger> counts = new HashMap<>();
+    final private Arbitrator arbitrator;
 
-  public TransactionWatcher(Arbitrator arbitrator) {
-    this.arbitrator = arbitrator;
-  }
-
-  public TransactionWatcher(ServerContext context) {
-    this(new ZooArbitrator(context));
-  }
-
-  public interface Arbitrator {
-    boolean transactionAlive(String type, long tid) throws Exception;
-
-    boolean transactionComplete(String type, long tid) throws Exception;
-  }
-
-  public static class ZooArbitrator implements Arbitrator {
-
-    private ServerContext context;
-    private ZooReader rdr;
-
-    public ZooArbitrator(ServerContext context) {
-      this.context = context;
-      rdr = new ZooReader(context.getZooKeepers(), context.getZooKeepersSessionTimeOut());
+    public TransactionWatcher(Arbitrator arbitrator) {
+        this.arbitrator = arbitrator;
     }
 
-    @Override
-    public boolean transactionAlive(String type, long tid) throws Exception {
-      String path = context.getZooKeeperRoot() + "/" + type + "/" + tid;
-      rdr.sync(path);
-      return rdr.exists(path);
+    public TransactionWatcher(ServerContext context) {
+        this(new ZooArbitrator(context));
     }
 
-    public static void start(ServerContext context, String type, long tid)
-        throws KeeperException, InterruptedException {
-      ZooReaderWriter writer = context.getZooReaderWriter();
-      writer.putPersistentData(context.getZooKeeperRoot() + "/" + type, new byte[] {},
-          NodeExistsPolicy.OVERWRITE);
-      writer.putPersistentData(context.getZooKeeperRoot() + "/" + type + "/" + tid, new byte[] {},
-          NodeExistsPolicy.OVERWRITE);
-      writer.putPersistentData(context.getZooKeeperRoot() + "/" + type + "/" + tid + "-running",
-          new byte[] {}, NodeExistsPolicy.OVERWRITE);
+    public interface Arbitrator {
+        boolean transactionAlive(String type, long tid) throws Exception;
+
+        boolean transactionComplete(String type, long tid) throws Exception;
     }
 
-    public static void stop(ServerContext context, String type, long tid)
-        throws KeeperException, InterruptedException {
-      ZooReaderWriter writer = context.getZooReaderWriter();
-      writer.recursiveDelete(context.getZooKeeperRoot() + "/" + type + "/" + tid,
-          NodeMissingPolicy.SKIP);
-    }
+    public static class ZooArbitrator implements Arbitrator {
 
-    public static void cleanup(ServerContext context, String type, long tid)
-        throws KeeperException, InterruptedException {
-      ZooReaderWriter writer = context.getZooReaderWriter();
-      writer.recursiveDelete(context.getZooKeeperRoot() + "/" + type + "/" + tid,
-          NodeMissingPolicy.SKIP);
-      writer.recursiveDelete(context.getZooKeeperRoot() + "/" + type + "/" + tid + "-running",
-          NodeMissingPolicy.SKIP);
-    }
+        private ServerContext context;
+        private ZooReader rdr;
 
-    public static Set<Long> allTransactionsAlive(ServerContext context, String type)
-        throws KeeperException, InterruptedException {
-      final ZooReader reader = context.getZooReaderWriter();
-      final Set<Long> result = new HashSet<>();
-      final String parent = context.getZooKeeperRoot() + "/" + type;
-      reader.sync(parent);
-      List<String> children = reader.getChildren(parent);
-      for (String child : children) {
-        if (child.endsWith("-running")) {
-          continue;
+        public ZooArbitrator(ServerContext context) {
+            this.context = context;
+            rdr = new ZooReader(context.getZooKeepers(), context.getZooKeepersSessionTimeOut());
         }
-        result.add(Long.parseLong(child));
-      }
-      return result;
+
+        @Override
+        public boolean transactionAlive(String type, long tid) throws Exception {
+            String path = context.getZooKeeperRoot() + "/" + type + "/" + tid;
+            rdr.sync(path);
+            return rdr.exists(path);
+        }
+
+        public static void start(ServerContext context, String type, long tid)
+                throws KeeperException, InterruptedException {
+            ZooReaderWriter writer = context.getZooReaderWriter();
+            writer.putPersistentData(context.getZooKeeperRoot() + "/" + type, new byte[]{},
+                    NodeExistsPolicy.OVERWRITE);
+            writer.putPersistentData(context.getZooKeeperRoot() + "/" + type + "/" + tid, new byte[]{},
+                    NodeExistsPolicy.OVERWRITE);
+            writer.putPersistentData(context.getZooKeeperRoot() + "/" + type + "/" + tid + "-running",
+                    new byte[]{}, NodeExistsPolicy.OVERWRITE);
+        }
+
+        public static void stop(ServerContext context, String type, long tid)
+                throws KeeperException, InterruptedException {
+            ZooReaderWriter writer = context.getZooReaderWriter();
+            writer.recursiveDelete(context.getZooKeeperRoot() + "/" + type + "/" + tid,
+                    NodeMissingPolicy.SKIP);
+        }
+
+        public static void cleanup(ServerContext context, String type, long tid)
+                throws KeeperException, InterruptedException {
+            ZooReaderWriter writer = context.getZooReaderWriter();
+            writer.recursiveDelete(context.getZooKeeperRoot() + "/" + type + "/" + tid,
+                    NodeMissingPolicy.SKIP);
+            writer.recursiveDelete(context.getZooKeeperRoot() + "/" + type + "/" + tid + "-running",
+                    NodeMissingPolicy.SKIP);
+        }
+
+        public static Set<Long> allTransactionsAlive(ServerContext context, String type)
+                throws KeeperException, InterruptedException {
+            final ZooReader reader = context.getZooReaderWriter();
+            final Set<Long> result = new HashSet<>();
+            final String parent = context.getZooKeeperRoot() + "/" + type;
+            reader.sync(parent);
+            List<String> children = reader.getChildren(parent);
+            for (String child : children) {
+                if (child.endsWith("-running")) {
+                    continue;
+                }
+                result.add(Long.parseLong(child));
+            }
+            return result;
+        }
+
+        @Override
+        public boolean transactionComplete(String type, long tid) throws Exception {
+            String path = context.getZooKeeperRoot() + "/" + type + "/" + tid + "-running";
+            rdr.sync(path);
+            return !rdr.exists(path);
+        }
     }
 
-    @Override
-    public boolean transactionComplete(String type, long tid) throws Exception {
-      String path = context.getZooKeeperRoot() + "/" + type + "/" + tid + "-running";
-      rdr.sync(path);
-      return !rdr.exists(path);
-    }
-  }
-
-  /**
-   * Run task only if transaction is still active in zookeeper. If the tx is no longer active then
-   * that task is not run and a debug message is logged indicating the task was ignored.
-   */
-
-  public void runQuietly(String ztxBulk, long tid, Runnable task) {
-
-    /*
-     * Lock l = stripedLocks.get(tid); l.lock(); try { if (!arbitrator.transactionAlive(ztxBulk,
-     * tid)) { log.debug("Transaction " + tid + " of type " + ztxBulk + " is no longer active.");
-     * return; } } catch (Exception e) { log.warn("Unable to check if transaction " + tid +
-     * " of type " + ztxBulk + " is alive ", e); return; } increment(tid); try { task.run(); }
-     * finally { decrement(tid); l.lock(); }
+    /**
+     * Run task only if transaction is still active in zookeeper. If the tx is no longer active then
+     * that task is not run and a debug message is logged indicating the task was ignored.
      */
 
-    synchronized (counts) {
+    public void runQuietly(String ztxBulk, long tid, Runnable task) {
+
+        Lock l = stripedLocks.get(tid);
+        l.lock();
+        try {
+            if (!arbitrator.transactionAlive(ztxBulk, tid)) {
+                log.debug("Transaction " + tid + " of type " + ztxBulk + " is no longer active.");
+                return;
+            }
+        } catch (Exception e) {
+            log.warn("Unable to check if transaction " + tid +
+                    " of type " + ztxBulk + " is alive ", e);
+            return;
+        }
+        increment(tid);
+        try {
+            task.run();
+        } finally {
+            decrement(tid);
+            l.lock();
+        }
+
+   /* synchronized (counts) {
       try {
         if (!arbitrator.transactionAlive(ztxBulk, tid)) {
           log.debug("Transaction " + tid + " of type " + ztxBulk + " is no longer active.");
@@ -160,26 +172,31 @@ public class TransactionWatcher {
     } finally {
       decrement(tid);
     }
+
+    */
   }
 
-  public <T> T run(String ztxBulk, long tid, Callable<T> callable) throws Exception {
+        public <T > T run(String ztxBulk, long tid, Callable<T > callable) throws Exception {
 
-    /*
-     * Lock l = stripedLocks.get(tid); l.lock();
-     * 
-     * if (!arbitrator.transactionAlive(ztxBulk, tid)) { throw new Exception("Transaction " + tid +
-     * " of type " + ztxBulk + " is no longer active"); } increment(tid);
-     * 
-     * try {
-     * 
-     * return callable.call();
-     * 
-     * } finally { decrement(tid); l.unlock();
-     * 
-     * }
-     */
+            Lock l = stripedLocks.get(tid);
+            l.lock();
 
-    synchronized (counts) {
+            if (!arbitrator.transactionAlive(ztxBulk, tid)) {
+                throw new Exception("Transaction " + tid + " of type " + ztxBulk + " is no longer active");
+            }
+            increment(tid);
+
+            try {
+
+                return callable.call();
+
+            } finally {
+                decrement(tid);
+                l.unlock();
+
+            }
+
+   /* synchronized (counts) {
       if (!arbitrator.transactionAlive(ztxBulk, tid)) {
         throw new Exception("Transaction " + tid + " of type " + ztxBulk + " is no longer active");
       }
@@ -189,47 +206,56 @@ public class TransactionWatcher {
       return callable.call();
     } finally {
       decrement(tid);
-    }
-  }
+    }*/
+        }
 
-  public boolean isActive(long tid) {
+        public boolean isActive ( long tid){
 
-    /*
-     * Lock l = stripedLocks.get(tid); l.lock(); try {
-     * 
-     * log.debug("Transactions in progress {}", counts); AtomicInteger count = counts.get(tid);
-     * return count != null && count.get() > 0;
-     * 
-     * } finally { l.unlock(); }
-     */
+            Lock l = stripedLocks.get(tid);
+            l.lock();
+            try {
 
-    synchronized (counts) {
+                log.debug("Transactions in progress {}", counts);
+                AtomicInteger count = counts.get(tid);
+                return count != null && count.get() > 0;
+
+            } finally {
+                l.unlock();
+            }
+
+   /* synchronized (counts) {
       log.debug("Transactions in progress {}", counts);
       AtomicInteger count = counts.get(tid);
       return count != null && count.get() > 0;
-    }
-  }
+    }*/
+        }
 
-  private void increment(long tid) {
-    AtomicInteger count = counts.get(tid);
-    if (count == null)
-      counts.put(tid, count = new AtomicInteger());
-    count.incrementAndGet();
-  }
+        private void increment ( long tid){
+            AtomicInteger count = counts.get(tid);
+            if (count == null)
+                counts.put(tid, count = new AtomicInteger());
+            count.incrementAndGet();
+        }
 
-  private void decrement(long tid) {
+        private void decrement ( long tid){
 
-    /*
-     * Lock l = stripedLocks.get(tid); l.lock(); try {
-     * 
-     * AtomicInteger count = counts.get(tid); if (count == null) {
-     * log.error("unexpected missing count for transaction {}", tid); } else { if
-     * (count.decrementAndGet() == 0) counts.remove(tid); }
-     * 
-     * } finally { l.unlock(); }
-     */
+            Lock l = stripedLocks.get(tid);
+            l.lock();
+            try {
 
-    synchronized (counts) {
+                AtomicInteger count = counts.get(tid);
+                if (count == null) {
+                    log.error("unexpected missing count for transaction {}", tid);
+                } else {
+                    if
+                    (count.decrementAndGet() == 0) counts.remove(tid);
+                }
+
+            } finally {
+                l.unlock();
+            }
+
+    /*synchronized (counts) {
       AtomicInteger count = counts.get(tid);
       if (count == null) {
         log.error("unexpected missing count for transaction {}", tid);
@@ -237,7 +263,7 @@ public class TransactionWatcher {
         if (count.decrementAndGet() == 0)
           counts.remove(tid);
       }
-    }
-  }
+    }*/
+        }
 
-}
+    }
